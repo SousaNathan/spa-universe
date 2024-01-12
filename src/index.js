4f96d765e47c68ee2c8b4b9f60e0e8046e035537
Nathan Sousa <nathan.wks@outlook.com>
2024-01-11 23:07:50 -0300

    escopo pages, rotas, modulos

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Router } from "./router.js";
+
+const router = new Router()
+
+router.add("/home", "/pages/home.html")
+router.add("/universo", "/pages/universo.html")
+router.add("/exploracao", "/pages/exploracao.html")
+router.add(404, "/pages/404.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
