4bd737e16f95bacae9f6e7b5fd9aec436974d147
Nathan Sousa <nathan.wks@outlook.com>
2024-01-12 01:06:06 -0300

    estilo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { Router } from "./router.js";
 
 const router = new Router()
 
-router.add("/home", "/pages/home.html")
+router.add("/", "/pages/home.html")
 router.add("/universo", "/pages/universo.html")
 router.add("/exploracao", "/pages/exploracao.html")
 router.add(404, "/pages/404.html")
